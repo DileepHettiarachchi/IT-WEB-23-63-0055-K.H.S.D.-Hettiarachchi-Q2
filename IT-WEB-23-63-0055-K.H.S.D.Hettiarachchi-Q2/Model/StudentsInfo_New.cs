1611481330b8dd14eda664843f1e04bafd5c4b48
DileepHettiarachchi <sandeepadhkh@gmail.com>
2024-05-24 21:03:44 +0530

    editedd

---
+++ IT-WEB-23-63-0055-K.H.S.D.Hettiarachchi-Q2/Model/StudentsInfo_New.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IT_WEB_23_63_0055_K.H.S.D.Hettiarachchi_Q2.Model
+{
+    public class StudentsInfo_New
+    {
+        public int Id { get; set; }
+        public string Student_No { get; set; }
+        public string Name { get; set; }
+        public String City { get; set; }
+        public String Course_Id { get; set; }
+
+        [ForeignKey("CoursesNewForeignKey")]
+        public int CoursesNewForeignKey { get; set; }
+
+        public CoursesNew CoursesNew { get; set; }
+    }
+}
--- IT-WEB-23-63-0055-K.H.S.D.Hettiarachchi-Q2/Pages/Students' Informations.cshtml.cs
+++ IT-WEB-23-63-0055-K.H.S.D.Hettiarachchi-Q2/Pages/Students' Informations.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IT_WEB_23_63_0055_K.H.S.D.Hettiarachchi_Q2.Pages
 {
-    public class Student_InfomationModel : PageModel
+    public class Index1Model : PageModel
     {
         public void OnGet()
         {
