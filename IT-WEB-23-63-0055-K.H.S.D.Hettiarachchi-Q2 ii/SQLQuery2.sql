60d6185ab668d92419eaeacf1e20bda8b5a1b98b
DileepHettiarachchi <163468118+DileepHettiarachchi@users.noreply.github.com>
2024-05-24 12:27:33 +0530

    Add files via upload

---
+++ IT-WEB-23-63-0055-K.H.S.D.Hettiarachchi-Q2 ii/SQLQuery2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Courses (
+Course_Id int NOT NULL,
+_Name varchar(255),
+Lecturer_Name varchar(255),
+PRIMARY KEY (Course_Id),
+)
---
+++ IT-WEB-23-63-0055-K.H.S.D.Hettiarachchi-Q2 ii/SQLQuery4.sql
@@ -0,0 +1,6 @@
+INSERT INTO Courses
+VALUES(
+4,
+'Java',
+'U.H.S. Perera'
+)
